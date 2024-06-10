d9646150a083b9eb739175ba1ec0edc1397eed30
amit2002d <amitgulati78@gmail.com>
2024-06-10 15:57:49 +0530

    message

--- pages/SFL.py
+++ pages/SFL.py
@@ -10,7 +10,7 @@ title = st.empty()
 df_place = st.empty()
 res_place = st.empty()
 st.session_state.last_analysis_time = time.time() - 110
-res = st.session_state.all_data["AAVAS"]
+res = st.session_state.all_data["SFL"]
 secrets = st.session_state.secrets
 if 'res' not in st.session_state:
     st.session_state.res = res
@@ -70,14 +70,14 @@ while True:
     if time.time() - st.session_state.last_analysis_time >= 100:
         st.session_state.last_analysis_time = time.time()
 
-        st.session_state.res['CMP'] = round(get_cmp_price(secrets["connections"]["gsheets"]["worksheets"]['AAVAS']),2)
+        st.session_state.res['CMP'] = round(get_cmp_price(secrets["connections"]["gsheets"]["worksheets"]['SFL']),2)
         st.session_state.res['Current Value'] = st.session_state.res['Qty.'] * st.session_state.res['CMP']
         st.session_state.res['Gain%'] = round(((res['Current Value'] - st.session_state.res['Buy Value']) / st.session_state.res['Buy Value']) * 100,2)
         st.session_state.res['Amount'] = st.session_state.res['Current Value'] - st.session_state.res['Buy Value']
 
         # st.session_state.res = res
         title.title('')
-        title.title(f'Data for AAVAS')
+        title.title(f'Data for SFL')
         res_place.text('')
         res_rounded = st.session_state.res.round(2)
         format_dict1 = {'Total Investment': '{:.2f}', 'Current Value': '{:.2f}', 'ROI': '{:.2f}',"AVG Price": '{:.2f}', "Qty": '{:.2f}','PnL': '{:.0f}'}
