698d8f8658792b41a7512c47d4fb912703b70d76
amit2002d <amitgulati78@gmail.com>
2025-05-19 20:43:32 +0530

    message

--- pages/EASEMYTRIP.py
+++ pages/EASEMYTRIP.py
@@ -4,6 +4,8 @@ import time
 import yfinance as yf
 from datetime import datetime
 import datetime
+import requests
+from bs4 import BeautifulSoup
 # from app import fetch_data_from_google_sheets, fetch_data_from_google_sheets_d
 
 title = st.empty()
@@ -15,12 +17,15 @@ secrets = st.session_state.secrets
 if 'res' not in st.session_state:
     st.session_state.res = res
 st.session_state.res = res
+
+
 def highlight_gain_condition(s):
     if s.name == 'Gain%':
         return s.apply(lambda x: highlight_single_gain(x))
     else:
         return [''] * len(s)
-    
+
+
 def highlight_condition(s):
     if s.name == 'ROI' or s.name == 'PnL':
         return s.apply(lambda x: highlight_single_gain(x))
@@ -29,12 +34,16 @@ def highlight_condition(s):
     else:
         return s.apply(lambda x: highlight_2(x))
 
+
 def highlight(x):
     color = 'rgba(139,190,27,1)'
     return 'background-color: %s' % color
+
+
 def highlight_2(x):
     color = 'rgba(255, 140, 0, 1)'
     return 'background-color: %s' % color
+
 
 def highlight_single_gain(value):
     if value < 0:
@@ -49,50 +58,65 @@ def highlight_single_gain(value):
         color = ''  # No highlighting if not in specified ranges
     return 'background-color: %s' % color
 
-def get_cmp_price(cmp_symbol):
-    try:
-        cmp_data = yf.Ticker(cmp_symbol+".NS")
-        cmp_price = cmp_data.history(period="1d")["Close"].iloc[-1]
-        return cmp_price
-    except Exception as e:
-        st.error(f"Failed to fetch cmp price: {e}")
-        return None
+
+def get_cmp_price(ticker):
+    url = f'https://www.google.com/finance/quote/{ticker}:NSE'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    class1 = "YMlKec fxKbKc"
+    return float(soup.find(class_=class1).text[1:].replace(',', ''))
+
+
 if 'Price' in st.session_state.res.columns and 'Qty.' in st.session_state.res.columns:
-    st.session_state.res['Price'] = st.session_state.res['Price'].str.replace(',', '').astype(float) if st.session_state.res['Price'].dtype == 'object' else st.session_state.res['Price']
-    st.session_state.res['Qty.'] = st.session_state.res['Qty.'].str.replace(',', '').astype(float) if st.session_state.res['Qty.'].dtype == 'object' else st.session_state.res['Qty.']
-    st.session_state.res['Buy Value'] = st.session_state.res['Price'] * st.session_state.res['Qty.']
-    st.session_state.res['Age'] = (datetime.datetime.now() - pd.to_datetime(res['Date'])).dt.days
+    st.session_state.res['Price'] = st.session_state.res['Price'].str.replace(',', '').astype(
+        float) if st.session_state.res['Price'].dtype == 'object' else st.session_state.res['Price']
+    st.session_state.res['Qty.'] = st.session_state.res['Qty.'].str.replace(',', '').astype(
+        float) if st.session_state.res['Qty.'].dtype == 'object' else st.session_state.res['Qty.']
+    st.session_state.res['Buy Value'] = st.session_state.res['Price'] * \
+        st.session_state.res['Qty.']
+    st.session_state.res['Age'] = (
+        datetime.datetime.now() - pd.to_datetime(res['Date'])).dt.days
 else:
     st.error("Columns 'Price' and/or 'Qty.' not found in the DataFrame.")
 
 while True:
-    df = pd.DataFrame(columns=['Total Investment','Current Value','ROI', "AVG Price", "Qty", 'PnL'])
+    df = pd.DataFrame(
+        columns=['Total Investment', 'Current Value', 'ROI', "AVG Price", "Qty", 'PnL'])
     if time.time() - st.session_state.last_analysis_time >= 100:
         st.session_state.last_analysis_time = time.time()
 
-        st.session_state.res['CMP'] = round(get_cmp_price(secrets["connections"]["gsheets"]["worksheets"]['EASEMYTRIP']),2)
-        st.session_state.res['Current Value'] = st.session_state.res['Qty.'] * st.session_state.res['CMP']
-        st.session_state.res['Gain%'] = round(((res['Current Value'] - st.session_state.res['Buy Value']) / st.session_state.res['Buy Value']) * 100,2)
-        st.session_state.res['Amount'] = st.session_state.res['Current Value'] - st.session_state.res['Buy Value']
+        st.session_state.res['CMP'] = round(get_cmp_price(
+            secrets["connections"]["gsheets"]["worksheets"]['EASEMYTRIP']), 2)
+        st.session_state.res['Current Value'] = st.session_state.res['Qty.'] * \
+            st.session_state.res['CMP']
+        st.session_state.res['Gain%'] = round(
+            ((res['Current Value'] - st.session_state.res['Buy Value']) / st.session_state.res['Buy Value']) * 100, 2)
+        st.session_state.res['Amount'] = st.session_state.res['Current Value'] - \
+            st.session_state.res['Buy Value']
 
         # st.session_state.res = res
         title.title('')
         title.title(f'Data for EASEMYTRIP')
         res_place.text('')
         res_rounded = st.session_state.res.round(2)
-        format_dict1 = {'Total Investment': '{:.2f}', 'Current Value': '{:.2f}', 'ROI': '{:.2f}',"AVG Price": '{:.2f}', "Qty": '{:.2f}','PnL': '{:.0f}'}
-        format_dict2 = {'Price': '{:.2f}', 'Qty.': '{:.2f}', 'CMP': '{:.2f}', 'Gain%': '{:.2f}', 'Amount': '{:.2f}', 'Buy Value': '{:.2f}', 'Current Value': '{:.2f}'}
+        format_dict1 = {'Total Investment': '{:.2f}', 'Current Value': '{:.2f}',
+                        'ROI': '{:.2f}', "AVG Price": '{:.2f}', "Qty": '{:.2f}', 'PnL': '{:.0f}'}
+        format_dict2 = {'Price': '{:.2f}', 'Qty.': '{:.2f}', 'CMP': '{:.2f}', 'Gain%': '{:.2f}',
+                        'Amount': '{:.2f}', 'Buy Value': '{:.2f}', 'Current Value': '{:.2f}'}
         total_place = st.empty()
         summary_place = st.empty()
         buy_value = st.session_state.res['Buy Value'].sum()
         current_value = st.session_state.res['Current Value'].sum()
-        roi = round(((current_value - buy_value) / buy_value) * 100,2)
+        roi = round(((current_value - buy_value) / buy_value) * 100, 2)
         gain = current_value - buy_value
         total_qty = st.session_state.res['Qty.'].sum()
         total_value = st.session_state.res['Buy Value'].sum()
-        avg_price = round(total_value / total_qty,2)
-        df = pd.DataFrame({'Total Investment': [buy_value], 'Current Value': [current_value], 'ROI': [roi], "AVG Price": [avg_price],'Qty': [total_qty],'PnL': [gain]})
-        styled_df = df.style.format(format_dict1).apply(highlight_condition, axis=0)
-        styled_res = res_rounded.sort_values('Date').style.format(format_dict2).apply(highlight_gain_condition, subset=['Gain%'], axis=0)
+        avg_price = round(total_value / total_qty, 2)
+        df = pd.DataFrame({'Total Investment': [buy_value], 'Current Value': [current_value], 'ROI': [
+                          roi], "AVG Price": [avg_price], 'Qty': [total_qty], 'PnL': [gain]})
+        styled_df = df.style.format(format_dict1).apply(
+            highlight_condition, axis=0)
+        styled_res = res_rounded.sort_values('Date').style.format(
+            format_dict2).apply(highlight_gain_condition, subset=['Gain%'], axis=0)
         df_place.dataframe(styled_df)
         res_place.dataframe(styled_res)
